66e5ffa8e3575de1af301dca81127c6a3534bdf0
ravidra-me <ravindrarajpoot9628172@gmail.com>
2021-04-04 22:20:58 +0530

    checkpoint

--- block-BNaafu/expense-tracker/app.js
+++ block-BNaafu/expense-tracker/app.js
@@ -11,11 +11,14 @@ var passport = require('passport');
 var flash =require('connect-flash');
 var auth = require('./middleware/auth');
 
+
 require('dotenv').config();
+require('./modules/passport');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var verifyemailRouter = require('./routes/verifyEmail');
+var authRouter = require('./routes/auth');
 
 
 
@@ -60,10 +63,15 @@ app.use(
 );
 
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(auth.userInfo);
 app.use("/", indexRouter);
 app.use('/verifyEmail', verifyemailRouter);
 app.use("/users", usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
